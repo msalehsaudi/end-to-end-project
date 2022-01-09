5f3bef7d95eccbdbf3dadb599632d97885825e61
Mohammad Saleh <m.saleh.saudi@gmail.com>
2022-01-09 20:33:35 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Forecast of Tesla stock using different time series methodologies mainly: SARIMA, LSTM, TCN, NBEATS, and others..',
+    author='Mohammad Saleh',
+    license='MIT',
+)
